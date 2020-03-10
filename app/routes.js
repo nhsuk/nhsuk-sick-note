50ad9244100bae2728f85b1ceecdafa311d4969d
AdamChrimes <iChrimes@live.co.uk>
2020-03-10 16:30:28 +0000

    add some basic screener question logic

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,41 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// Branching example
+router.post('/screener-question/answer', function (req, res) {
+
+  // Make a variable and give it the value from 'know-nhs-number'
+  var answer = req.session.data['self-isolate']
+
+  // Check whether the variable matches a condition
+  if (answer == "yes"){
+    // Send user to next page
+    res.redirect('/screener-question-yes')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/screener-question-no')
+  }
+
+})
+
+// Branching example
+router.post('/screener-question-date/answer', function (req, res) {
+
+  // Make a variable and give it the value from 'know-nhs-number'
+  var answer = req.session.data['self-isolate-date']
+
+  // Check whether the variable matches a condition
+  if (answer == "yes"){
+    // Send user to next page
+    res.redirect('/what-is-your-name')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/screener-question-date-no')
+  }
+
+})
+
 module.exports = router;
+
